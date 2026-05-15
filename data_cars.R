cdd03adca7b714739c01ba84b3d1d6c864a6ffe3
divani <vanileli142@gmail.com>
2026-05-15 15:28:07 +0700

    classify mtcars mpg based on mean value

--- data_cars.R
+++ data_cars.R
@@ -11,3 +11,12 @@ for (i in 1:n){
 }
 
 hist(mtcars$mpg)
+
+# 1. Menampilkan data kolom mpg dari dataframe mtcars
+mtcars$mpg
+# 2. Menghitung rata-rata dari kolom mpg
+mean_mpg = mean(mtcars$mpg)
+# 3. Membuat kolom baru 'upper_mean' di dalam dataframe mtcars
+mtcars$upper_mean = ifelse(mtcars$mpg >= mean_mpg, "upper", "lower")
+# 4. Melakukan verifikasi hasil (opsional)
+head(mtcars[, c("mpg", "upper_mean")])
